17e53869e1802f0514c6ccad1c5e727008f63034
James-Beans <spicey949@gmail.com>
2025-10-19 14:04:29 +0100

    Command Updates

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # core
-The main beansmc server core custom plugin.
+> The main beansmc server core custom plugin.
+
+---
+
+## Building
+To build `beansmc/core` to use in your own server we recommend using [IntelliJ IDEA Community Edition]() to build the `core-{VERSION}.jar` file through Maven.
+
+1. Clone the repo using either: `gh repo clone beansmc/core` or `git clone https://github.com/beansmc/core`
+2. Open IntelliJ IDEA Community Edition in the core folder we just cloned
+3. Click on the hamburger icon (4 lines on top of each other) in the top left
+4. Click on Build -> Build Project
+5. Wait and then once that is done click on the Maven symbol (a lowercase `m`) on the right bar
+6. Select core and then press the green arrow/play button to build
+7. Wait a minute...
+8. The `target` folder will be created and the `core-{VERSION}.jar` file will be created inside it
+
+To play with this `core-{VERSION}.jar` file drag it into a [Paper](https://papermc.io/software/paper/) server's `plugins` folder, run the server and join the server through Minecraft.
+
+---
+
+## Commands
+- `/core` - The main command.
+```
+<br><br><shadow:black>                     <dark_gray>---</dark_gray> <gray>/</gray><gradient:#F70E4D:#F12760>Core</gradient> <dark_gray>---</dark_gray> </shadow><br>       The main <shadow:black><gradient:#F95A22:#FFA200>Beans</gradient><gray>-</gray><color:#D4DDE0>m</color><color:#dead50>c</color></shadow> server <shadow:black><gradient:#F70E4D:#F12760>Core</gradient></shadow> plugin.<br>                    <shadow:black> <dark_gray>------------</dark_gray> </shadow><br><shadow:black>                   <gray>/</gray><gradient:#F70E4D:#F12760>Core</gradient> help <gray>{page} </shadow><br><shadow:black>                <gray>/</gray><gradient:#F70E4D:#F12760>Core</gradient> player <gray>{player} </shadow><br><br>
+```
+
+- `/core help {page}` - The help pages.
+```
+<br><br><shadow:black>              <dark_gray>---</dark_gray> <gray>/</gray><gradient:#F70E4D:#F12760>Core</gradient> help <gray>{page}</gray> <dark_gray>---</dark_gray> </shadow><br><br><shadow:black>                <gray>/</gray><gradient:#F70E4D:#F12760>Core</gradient> player <gray>{player} </shadow><br>         Shows info about a specific player.<br>  <br>           <shadow:black> <dark_gray>----------------------</dark_gray> </shadow><br><br><br>
+```
+
+- `/core player {player}` - Shows info about a specific player.
+```
+<br><br><shadow:black>           <dark_gray>---</dark_gray> <gray>/</gray><gradient:#F70E4D:#F12760>Core</gradient> player <gray>{player}</gray> <dark_gray>---</dark_gray> </shadow><br><shadow:black>                     <yellow><player_name></yellow> </shadow><br><br>                     <player_hp><red>♥</red><br>                  <player_location><blue>✈</blue><br><br>         <shadow:black> <dark_gray>---------------------------</dark_gray> </shadow><br><br><br>
+```
+
--- src/main/java/org/moniti/core/commands/cc.java
+++ src/main/java/org/moniti/core/commands/cc.java
@@ -1,12 +1,18 @@
 package org.moniti.core.commands;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.event.HoverEvent;
+import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class cc implements CommandExecutor {
@@ -18,32 +24,49 @@ public class cc implements CommandExecutor {
         // --- Command Logic ---
 
         // Console Check (always good to have for player-specific commands)
-        if (!(sender instanceof org.bukkit.entity.Player)) {
-            Component consoleMessage = miniMessage.deserialize("<gold>The core command was executed by the console. Arguments: <yellow>" + String.join(" ", args));
-            sender.sendMessage(consoleMessage);
+        if (args.length == 0 && !(sender instanceof Player)) {
+            // Allow console to see the main message
+            sendMainMessage(sender);
             return true;
         }
 
-        // Check for sub-commands (e.g., /core help)
-        if (args.length > 0 && args[0].equalsIgnoreCase("help")) {
-            sendHelpMessage(sender);
-            return true;
+        // Handle sub-commands (e.g., /core help)
+        if (args.length > 0) {
+            String subCommand = args[0].toLowerCase();
+
+            // 1. /core help
+            if (subCommand.equals("help")) {
+                sendHelpMessage(sender);
+                return true;
+            }
+
+            // 2. /core player {player}
+            else if (subCommand.equals("player")) {
+                if (args.length < 2) {
+                    sender.sendMessage(miniMessage.deserialize("Usage: /core player {player}"));
+                    return true;
+                }
+                String targetPlayerName = args[1];
+                sendPlayerInfoMessage(sender, targetPlayerName);
+                return true;
+            }
         }
 
-        // A. Define the MiniMessage string for the main /core command.
-        String miniMessageFormat =
-            "<gray>[<gradient:dark_red:gold>CORE</gradient>]</gray> " +
-            "Welcome to the <#55FF55>Core</#55FF55> Plugin! <br>" +
-            "<hover:show_text:'<yellow>Click to view help!'> " +
-            "<click:run_command:'/core help'>[<aqua>Help</aqua>]</click></hover>";
-
-        // B. Deserialize the MiniMessage string into an Adventure Component object.
-        Component finalMessage = miniMessage.deserialize(miniMessageFormat);
-
-        // C. Send the Component to the Player
-        sender.sendMessage(finalMessage);
+        // Default action (no subcommand or unrecognized subcommand)
+        sendMainMessage(sender);
 
         return true;
+    }
+
+    /**
+     * Sends a detailed main message to the CommandSender using MiniMessage.
+     * @param sender The entity to send the message to.
+     */
+    private void sendMainMessage(@NotNull CommandSender sender) {
+        String mainMessage =
+                "<br><br><shadow:black>                     <dark_gray>---</dark_gray> <gray>/</gray><gradient:#F70E4D:#F12760>Core</gradient> <dark_gray>---</dark_gray> </shadow><br>       The main <shadow:black><gradient:#F95A22:#FFA200>Beans</gradient><gray>-</gray><color:#D4DDE0>m</color><color:#dead50>c</color></shadow> server <shadow:black><gradient:#F70E4D:#F12760>Core</gradient></shadow> plugin.<br>                    <shadow:black> <dark_gray>------------</dark_gray> </shadow><br><shadow:black>                   <gray>/</gray><gradient:#F70E4D:#F12760>Core</gradient> help <gray>{page} </shadow><br><shadow:black>                <gray>/</gray><gradient:#F70E4D:#F12760>Core</gradient> player <gray>{player} </shadow><br><br>";
+
+        sender.sendMessage(miniMessage.deserialize(mainMessage));
     }
 
     /**
@@ -52,12 +75,51 @@ public class cc implements CommandExecutor {
      */
     private void sendHelpMessage(@NotNull CommandSender sender) {
         String helpMessage =
-                "<dark_gray>--- <gradient:gold:yellow>Core Plugin Help</gradient> ---<br>" +
-                "<gold>/core</gold> <gray>- Shows the main plugin info.<br>" +
-                "<gold>/core help</gold> <gray>- Displays this help menu.<br>" +
-                "<gold>/core status</gold> <gray>- Check the plugin's status. (Command to be added later!)<br>" +
-                "<gray>---------------------------";
+                "<br><br><shadow:black>              <dark_gray>---</dark_gray> <gray>/</gray><gradient:#F70E4D:#F12760>Core</gradient> help <gray>{page}</gray> <dark_gray>---</dark_gray> </shadow><br><br><shadow:black>                <gray>/</gray><gradient:#F70E4D:#F12760>Core</gradient> player <gray>{player} </shadow><br>         Shows info about a specific player.<br>  <br>           <shadow:black> <dark_gray>----------------------</dark_gray> </shadow><br><br><br>";
 
         sender.sendMessage(miniMessage.deserialize(helpMessage));
+    }
+
+    /**
+     * Sends player information to the CommandSender using MiniMessage.
+     * This demonstrates fetching basic player data and using MiniMessage placeholders.
+     * @param sender The entity receiving the message.
+     * @param targetPlayerName The name of the player to look up.
+     */
+    private void sendPlayerInfoMessage(@NotNull CommandSender sender, @NotNull String targetPlayerName) {
+        Player target = Bukkit.getPlayer(targetPlayerName);
+
+        if (target == null) {
+            sender.sendMessage(miniMessage.deserialize("Player <yellow>" + targetPlayerName + "</yellow> is <red>not</red> currently online."));
+            return;
+        }
+
+        String nameMCUrl = "https://namemc.com/profile/" + target.getName();
+
+        Component clickableNameComponent = Component.text(target.getName())
+                .color(NamedTextColor.GOLD)
+                .clickEvent(ClickEvent.openUrl(nameMCUrl))
+                .hoverEvent(HoverEvent.showText(
+                        miniMessage.deserialize("<gray>Click to view <yellow>" + target.getName() + "</yellow>'s NameMC profile")
+                ));
+
+        String infoMessage =
+                "<br><br><shadow:black>           <dark_gray>---</dark_gray> <gray>/</gray><gradient:#F70E4D:#F12760>Core</gradient> player <gray>{player}</gray> <dark_gray>---</dark_gray> </shadow><br><shadow:black>                     <yellow><player_name></yellow> </shadow><br><br>                     <player_hp><red>♥</red><br>                  <player_location><blue>✈</blue><br><br>         <shadow:black> <dark_gray>---------------------------</dark_gray> </shadow><br><br><br>";
+
+        Component message = miniMessage.deserialize(
+                infoMessage,
+
+                Placeholder.component("player_name", clickableNameComponent),
+                Placeholder.unparsed("player_uuid", target.getUniqueId().toString()),
+                Placeholder.unparsed("player_hp", String.format("%.1f", target.getHealth())),
+                Placeholder.unparsed("player_location", String.format("%s (%.0f, %.0f, %.0f)",
+                        target.getWorld().getName(),
+                        target.getLocation().getX(),
+                        target.getLocation().getY(),
+                        target.getLocation().getZ()
+                ))
+        );
+
+        sender.sendMessage(message);
     }
 }
